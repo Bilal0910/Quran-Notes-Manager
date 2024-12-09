573ca3a5562fee9fe15a0f7d780d65f6f74648bd
Bilal0910 <bilalnadeem4u@gmail.com>
2024-12-09 23:32:29 +0500

    CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const cors = require("cors"); // Import CORS
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 require("./db/mongoose");
-require('dotenv').config({ path: './config/.env' });
+require("dotenv").config({ path: "./config/.env" });
 
 // Define express app and port
 const app = express();
-const port = process.env.PORT 
+const port = process.env.PORT;
+
+// Use CORS middleware
+app.use(cors());
+
 
 app.use(express.json());
 app.use(userRouter);
@@ -16,4 +21,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
-
